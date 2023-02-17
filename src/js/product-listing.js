6739edcea1e0029abb206f51f16531d0c36d2fa5
livia-pimentel <livia_macedo89@yahoo.com.br>
2023-02-17 01:26:46 -0300

    week 07 part 3

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,10 +1,10 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import {loadHeaderFooter, getParam} from "./utils.mjs";
 
 
 const category = getParam("category")
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const element = document.querySelector(".product-list")
 const productList = new ProductList(category,dataSource,element);
 productList.init()
